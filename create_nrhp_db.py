ae2876c6bfb5e0eb39d5a8b45049aff913a17a71
harrisonglotz <harrisonglotz@gmail.com>
2020-05-03 22:33:23 -0400

    added ability to download current spreasheet from web

--- create_nrhp_db.py
+++ create_nrhp_db.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 import pandas as pd
 import sqlite3
+import requests
+
+#get latest verison of the spreadsheet and download
+url = 'https://www.nps.gov/subjects/nationalregister/upload/national_register_listed_20200108.xlsx'
+r = requests.get(url, allow_redirects=True)
+open('nrhp.xlsx', 'wb').write(r.content)
 
 #name database
 dbname = 'nrhp'
@@ -13,4 +19,3 @@ df = pd.read_excel('nrhp.xlsx')
 
 #convert dataframe into a sqlite file
 df.to_sql(name='AdvSearchResults', con=conn)
-
