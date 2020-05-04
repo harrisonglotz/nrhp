7f62332fa1d140c7c3dd463eb1c257185c1dd9d7
harrisonglotz <harrisonglotz@gmail.com>
2020-05-04 11:40:35 -0400

    added automatic column naming

--- create_nrhp_db.py
+++ create_nrhp_db.py
@@ -19,3 +19,25 @@ df = pd.read_excel('nrhp.xlsx')
 
 #convert dataframe into a sqlite file
 df.to_sql(name='AdvSearchResults', con=conn)
+
+#rename colums rows so there are no spaces
+cur = conn.cursor()
+cur.execute("ALTER TABLE 'AdvSearchResults' RENAME COLUMN 'Property Name' TO 'Property_Name'") 
+cur.execute("ALTER TABLE 'AdvSearchResults' RENAME COLUMN 'Listed Date' TO 'Listed_Date'")
+cur.execute("ALTER TABLE 'AdvSearchResults' RENAME COLUMN 'Ref#' TO 'Ref_Num'")
+cur.execute("ALTER TABLE 'AdvSearchResults' RENAME COLUMN 'NHL Designated Date' TO 'NHL_Designated_Date'")
+cur.execute("ALTER TABLE 'AdvSearchResults' RENAME COLUMN 'Restricted Address' TO 'Restricted_Address'")
+cur.execute("ALTER TABLE 'AdvSearchResults' RENAME COLUMN 'Name of Multiple Property Listing' TO 'Name_Of_Multiple_Property_Listing'")
+cur.execute("ALTER TABLE 'AdvSearchResults' RENAME COLUMN 'Street & Number' TO 'Street_And_Number'")
+cur.execute("ALTER TABLE 'AdvSearchResults' RENAME COLUMN 'Federal Agencies' TO 'Federal_Agencies'")
+cur.execute("ALTER TABLE 'AdvSearchResults' RENAME COLUMN 'Level of Significance - Local' TO 'Level_Of_Significance_Local'")
+cur.execute("ALTER TABLE 'AdvSearchResults' RENAME COLUMN 'Level of Significance - State' TO 'Level_Of_Significance_State'")
+cur.execute("ALTER TABLE 'AdvSearchResults' RENAME COLUMN 'Level of Significance - National' TO 'Level_Of_Significance_National'")
+cur.execute("ALTER TABLE 'AdvSearchResults' RENAME COLUMN 'Level of Significance - International' TO 'Level_Of_Significance_International'")
+cur.execute("ALTER TABLE 'AdvSearchResults' RENAME COLUMN 'Level of Significance - Not Indicated' TO 'Level_Of_Significance_Not_Indicated'")
+cur.execute("ALTER TABLE 'AdvSearchResults' RENAME COLUMN 'Other Names' TO 'Other_Names'")
+cur.execute("ALTER TABLE 'AdvSearchResults' RENAME COLUMN 'Park Name' TO 'Park_Name'")
+cur.execute("ALTER TABLE 'AdvSearchResults' RENAME COLUMN 'Significant Persons' TO 'Significant_Persons'")
+cur.execute("ALTER TABLE 'AdvSearchResults' RENAME COLUMN 'External Link' TO 'External_Link'")
+
+
