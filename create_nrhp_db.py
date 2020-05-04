c2dafc39aeefd7dce47fc43d4a48705af07ee9fa
harrisonglotz <55510883+harrisonglotz@users.noreply.github.com>
2020-05-03 21:38:55 -0400

    Update create_nrhp_db.py

--- create_nrhp_db.py
+++ create_nrhp_db.py
@@ -11,6 +11,6 @@ conn = sqlite3.connect(dbname + '.sqlite')
 #read excel file with pandas
 df = pd.read_excel('nrhp.xlsx')
 
-#convert datafram into a sqlite file
+#convert dataframe into a sqlite file
 df.to_sql(name='AdvSearchResults', con=conn)
 
