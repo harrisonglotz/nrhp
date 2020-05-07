8ee04773ba8376103677afa16fd5d9750aa1070d
harrisonglotz <harrisonglotz@gmail.com>
2020-05-06 20:20:58 -0400

    fixed bug where two word cities didn't return listings

--- search_nrhp.py
+++ search_nrhp.py
@@ -24,7 +24,7 @@ user_input_county = user_input_county.title()
 #get user's city, if using, and format for query. Query and printing can use the same variable here, no need for two variables
 user_input_city = input('Enter city (leave blank if county): ')
 user_input_city = user_input_city.lower()
-user_input_city = user_input_city.capitalize()
+user_input_city = user_input_city.title()
 
 if len(user_input_city) == 0:
 	#get the amount of listings that match the search/query criteria 
@@ -64,7 +64,7 @@ else:
 	#query relevant data and sort by Property_Name
 	cur.execute(f'''SELECT Property_Name, External_Link, Street_And_Number FROM AdvSearchResults
 				WHERE State='{user_input_state}' AND City='{user_input_city}' OR City='{user_input_city} (Independent City)' 
-				ORDER BY Property_Name ASC''')
+				ORDER BY Property_Name''')
 	i = 1
 	counter = 0
 	name_list = ["Placeholder"]
@@ -86,36 +86,38 @@ cur.close()
 
 #prompt user to select a listing and turn into int
 listing_selection = input("\nEnter listing # for more information: ")
-listing_selection = int(listing_selection)
+if len(listing_selection) == 0:
+	pass
+else:
+	listing_selection = int(listing_selection)
+	#assign variables based on selection
+	selected_name = name_list[listing_selection]
+	selected_address = address_list[listing_selection]
+	selected_external_link = link_list[listing_selection]
 
-#assign variables based on selection
-selected_name = name_list[listing_selection]
-selected_address = address_list[listing_selection]
-selected_external_link = link_list[listing_selection]
+	print(f"\nInformation for {selected_name}:") 
+	print(f"Address/Location: {selected_address}")
+	print(f"External Link: {selected_external_link}\n")
 
-print(f"\nInformation for {selected_name}:") 
-print(f"Address/Location: {selected_address}")
-print(f"External Link: {selected_external_link}\n")
+	#test address mapping capability 
+	test_string = selected_address[0:2]
 
-#test address mapping capability 
-test_string = selected_address[0:2]
+	if test_string.isdigit():
+		map_decision = input("Map it on Google Maps? (y/n): ")
+		if map_decision == 'y':
+			if len(user_input_county) == 0:
+				is_county = False
+			else:
+				is_county = True
 
-if test_string.isdigit():
-	map_decision = input("Map it on Google Maps? (y/n): ")
-	if map_decision == 'y':
-		if len(user_input_county) == 0:
-			is_county = False
+			if is_county:
+				webbrowser.open(f"https://google.com/maps/place/{selected_address} {user_input_county} county, {user_input_state}")
+			else:
+				webbrowser.open(f"https://google.com/maps/place/{selected_address} {user_input_city}, {user_input_state}")
 		else:
-			is_county = True
-
-		if is_county:
-			webbrowser.open(f"https://google.com/maps/place/{selected_address} {user_input_county} county {user_input_state}")
-		else:
-			webbrowser.open(f"https://google.com/maps/place/{selected_address} {user_input_city} {user_input_state}")
+			pass
 	else:
 		pass
-else:
-	pass
 
 
 
